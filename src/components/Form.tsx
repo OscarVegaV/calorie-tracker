4f86b264ac778061b5bb51045fd292ebb0722f06
unknown <os_ar1@hotmail.com>
2025-06-21 08:33:53 -0600

    feat: add input form, activity containers, and state management setup

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+import type { ChangeEvent } from "react"
+import  type { Activity } from "../types"
 import { categories } from "../data/categories"
 
 export default function Form() {
+
+    const [activity, setActivity] = useState<Activity>({
+        category: 1, // Default category ID
+        nameActivity: '',
+        calories: 0
+    })
+
+    const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement> ) => {
+        const isNumberField = ['category', 'calories'].includes(e.target.id);
+
+        console.log(isNumberField);
+        
+       setActivity({
+            ...activity,
+          [e.target.id]: isNumberField ? +e.target.value : e.target.value
+
+        })
+    }
+
   return (
     <form
       className="space-y-5 bg-white shadow p-10 rounded-lg"
@@ -10,6 +32,8 @@ export default function Form() {
             <select
                 className=" border border-slate-300 rounded-lg w-full bg-white"
                 id="category"
+                value={activity.category}
+                onChange={handleChange}
             >
                 {categories.map(category => (
                     <option key={category.id} value={category.id}>
@@ -22,10 +46,14 @@ export default function Form() {
         </div>
 
         <div className="grid grid-cols-1 gap-3">    
-            <label htmlFor="activity" className="font-bold">Activity:</label>
+            <label htmlFor="nameActivity" className="font-bold">Activity:</label>
             <input
-                id="activity" type="text" className="border border-slate-300 p-2 rounded-lg"
+                id="nameActivity" 
+                type="text" 
+                className="border border-slate-300 p-2 rounded-lg"
                 placeholder="Example: food, orange juice, salad, exercise, running, cycling, etc."
+                value={activity.nameActivity}
+                onChange={handleChange}
              />    
 
         </div>
@@ -33,8 +61,12 @@ export default function Form() {
         <div className="grid grid-cols-1 gap-3">    
             <label htmlFor="calories" className="font-bold">Calories: </label>
             <input
-                id="calories" type="number" className="border border-slate-300 p-2 rounded-lg"
+                id="calories" 
+                type="number" 
+                className="border border-slate-300 p-2 rounded-lg"
                 placeholder=" Calories consumed or burned example: 200, 500, etc."
+                value={activity.calories}
+                onChange={handleChange}
              />    
 
         </div>
